01845e731e9dfa15ea01125f33885240b46957a0
Andreas Bernacca <hello@rinti.se>
2019-05-11 10:57:49 +0200

    In with the new

---
+++ brew.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+brew update
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+brew install gnupg
+brew install neovim
+brew install grep
+brew install openssh
+brew install git
+brew install git-flow
+brew install tree
+
+brew cleanup
---
+++ setup.sh
@@ -0,0 +1,20 @@
+# Remove date at top of terminal
+touch ~/.hushlogin
+
+# Git
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+# Bash
+ln -sf ~/dotfiles/.bash_prompt ~/.bash_prompt
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+
+# Nvm
+mkdir ~/.nvm 2> /dev/null || true
+
+# Nvim
+mkdir ~/.config 2> /dev/null || true && mkdir ~/.config/nvim 2> /dev/null || true
+ln -sf ~/dotfiles/.nvimrc ~/.config/nvim/init.vim
+mkdir ~/.cache 2> /dev/null || true && mkdir ~/.cache/dein 2> /dev/null || true
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
