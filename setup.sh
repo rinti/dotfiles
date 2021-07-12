c9fc825bfddf591823889bb9d2485698593e4f3c
Andreas Bernacca <hello@rinti.se>
2021-07-12 15:20:02 +0200

    Move nvim folder to dotfiles and symlink the folder

--- setup.sh
+++ setup.sh
@@ -21,16 +21,11 @@ ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
 ln -sf ~/dotfiles/.fdignore ~/.fdignore
 
 # Nvm
-mkdir ~/.nvm 2> /dev/null || true
+mkdir -p ~/.nvm 2> /dev/null || true
 
 # Nvim
 mkdir -p ~/.config/nvim 2> /dev/null || true
-ln -sf ~/dotfiles/init.lua ~/.config/nvim/init.lua
-
-# vimplug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+ln -sf ~/dotfiles/nvim ~/.config/nvim
 
 # tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
