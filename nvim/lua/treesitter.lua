23d7385d40896a988f75492e27b5e260931677ff
Andreas Bernacca <hello@rinti.se>
2026-04-27 10:49:00 +0200

    Enable treesitter highlighting on FileType in nvim

--- nvim/lua/treesitter.lua
+++ nvim/lua/treesitter.lua
@@ -1,3 +1,8 @@
 -- Treesitter
--- Highlighting is built-in to Neovim 0.12, nvim-treesitter just installs parsers
-require('nvim-treesitter').install { "python", "javascript", "typescript", "html", "css", "markdown", "markdown_inline" }
+-- nvim-treesitter v1 only installs parsers; highlighting must be started per buffer
+require('nvim-treesitter').install { "python", "javascript", "typescript", "tsx", "html", "css", "markdown", "markdown_inline" }
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'python', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'html', 'css', 'markdown' },
+  callback = function() pcall(vim.treesitter.start) end,
+})
